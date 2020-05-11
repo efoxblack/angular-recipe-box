27ae070f9f63e4f04fec63a71849351c91578645
efoxblack <erikmichaelfox@gmail.com>
2020-05-11 16:25:01 -0400

    New componenetgit add .git add .!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import { FootersComponent } from './components/footers/footers.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { CardDetailsComponent } from './components/card-details/card-details.component';
 import { PopUpService } from './components/services/pop-up.service';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FootersComponent,
     CardsComponent,
-    CardDetailsComponent
+    CardDetailsComponent,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
