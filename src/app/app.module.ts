005cfbb115111ef475dc8101eaf8be768c5795eb
Ondia Brown <Ondianeveah19@gmail.com>
2020-05-11 13:24:13 -0700

    added demo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddHeaderComponentComponent } from './components/add-header-component/add-header-component.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddHeaderComponentComponent
+    AddHeaderComponentComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
